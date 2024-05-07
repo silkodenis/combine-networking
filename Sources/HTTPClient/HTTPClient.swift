21537b973eda6255d86e3115afe57d4f7c4ae8d0
Denis Silko <silkodenis@gmail.com>
2024-05-07 23:12:35 +0300

    Update access modifiers

--- Sources/HTTPClient/HTTPClient.swift
+++ Sources/HTTPClient/HTTPClient.swift
@@ -17,6 +17,6 @@
 import Foundation
 import Combine
 
-public protocol HTTPClient {
+internal protocol HTTPClient {
     func execute<T: Decodable>(_ request: URLRequest) -> AnyPublisher<T, Error>
 }
